21f36e769f78a2d298505fb4cf68704cfa50e7cb
justas <justas@192.168.1.38>
2013-02-06 01:44:09 +0400

    wip

--- mobatha/src/Model/IProject.java
+++ mobatha/src/Model/IProject.java
@@ -1,5 +1,11 @@
 package Model;
 
-public interface IProject {
+import java.util.HashSet;
 
+public interface IProject {
+	public String getId();
+	public void setId(String id);
+	public String getName();
+	public void setName(String name);
+	public HashSet<WorkTool> getWorkingTools();
 }
--- mobatha/src/Model/Project.java
+++ mobatha/src/Model/Project.java
@@ -1,5 +1,13 @@
 package Model;
 
+import java.util.HashSet;
+
 public class Project extends BaseObject implements IProject {
+	private HashSet<WorkTool> workTools = new HashSet<WorkTool>();
+	
+	@Override
+	public HashSet<WorkTool> getWorkingTools() {
+		return workTools;
+	}
 
 }
